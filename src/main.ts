858b83cbd0f2b6b55cc729c53e7c501b525066b0
BrunoAlvesTavares <brunoalvestavares@hotmail.com>
2023-11-13 21:46:46 -0300

    feat: ajustando backend

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
-import * as http from 'http';
-import * as httpProxy from 'http-proxy';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Configura o cors para permitir solicitações da porta 80
-  app.use(cors({
-    origin: 'http://localhost:80',
-    optionsSuccessStatus: 200,
-  }));
+  app.use(cors()); // adiciona o middleware cors
 
-  // Inicia o servidor na porta 3333
-  await app.listen(3333);
-
-  // Cria o proxy para redirecionar para a porta 3333
-  const proxy = httpProxy.createProxyServer({});
-  const proxyServer = http.createServer((req, res) => {
-    proxy.web(req, res, { target: 'http://localhost:3333' });
-  });
-
-  // Inicia o proxy na porta 80
-  proxyServer.listen(80, () => {
-    console.log('Proxy rodando na porta 80');
-  });
+  await app.listen(process.env.PORT);
 }
-
 bootstrap();
